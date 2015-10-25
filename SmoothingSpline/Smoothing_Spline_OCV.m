06e86eb93eae2dd7963369bd1a2a4376b9d52aef
duvvurisubramanyam <duvvurisubramanyam89@gmail.com>
2015-10-25 21:59:19 +0100

    commit

--- SmoothingSpline/Smoothing_Spline_OCV.m
+++ SmoothingSpline/Smoothing_Spline_OCV.m
@@ -1,10 +1,15 @@
 %New script using functions to shorten the code
 clc
 clear
-nSensors = 100;
+fprintf('Enter a function for consideration')
+fprintf (['    1-->y = 2*exp(-0.4*(x-2)^2) + 5/(x+10) + 0.1*x -0.2' ...
+            '\n  2-->val = 3^i - 2^i + exp(-5*i) + exp (-20 * (i-.5)^2)' , ...
+            ' \n 3-->val = 4.26 * (exp(-i)-4 * exp (-2*i) +3 * exp (-3 *i))']);
+ option = input ('\n>>');
+nSensors = 100; 
 noise = 0.1;
-Start_point =-5;
-End_point = 10;
+Start_point =0;
+End_point =1;
 knotspan=knot_calculation (nSensors,Start_point,End_point); %Automatic Claculation of Knot Span --> Rupert Extimation min(n/4,40)
 knots = Start_point:knotspan:End_point;
 xMin = knots(1);
@@ -17,7 +22,7 @@ yVec = NaN(xLen,1);
 add_spline = 0;
 add_derv=0;
 %lambda=.005;
-lambda=[0.1,.2,.5 ];
+lambda=[0.001,.02,.005 ];
 sum_Error= 0;
 Grid_opt =.001;
 RMS = 0;
@@ -28,19 +33,19 @@ lastKnot =knots(end);
 %=======================
 %------------------------------------------
 for i=1:xLen
-    yVec(i) = dummyCurve(xVec(i));
+    yVec(i) = dummyCurve(xVec(i),option);
 end
-plot(xVec, yVec,'g--','LineWidth',3);
-legend ('nknots');
-hold on;
+%plot(xVec, yVec,'g--','LineWidth',3);
+%legend ('nknots');
+%hold on;
 load DataRandom1000;  %randomXpositions  randomYpositions  randomZ   
 xSensors = xMin + (xMax-xMin)* (0.5 *randomXpositions(5:nSensors+4)+0.5);  % ignore first 4 samples in random list, due to fixed postions
 xSensors = sort(xSensors); %Determining X posotion of sensors
 ySensors = NaN(nSensors,1);
 for i=1:nSensors
-      ySensors(i)=dummyCurve(xSensors(i)) + noise*randomZ(i); %Determining Y posotion of sensors
+      ySensors(i)=dummyCurve(xSensors(i),option) + noise*randomZ(i); %Determining Y posotion of sensors
 end
-plot(xSensors, ySensors, 'mo','MarkerFaceColor',[.10 1 .63]);
+%plot(xSensors, ySensors, 'mo','MarkerFaceColor',[.10 1 .63]);
 %--------------------------------------------------------
 %CALCULATING THE ESTIMATION
 %============================
--- adding_functions/knot_calculation.m
+++
@@ -1,7 +0,0 @@
-function KnotSpan = knot_calculation( nSensors ,Start , End  )
-    knots_number = min(nSensors/4 , 40);
-    range = [Start : End];
-   range_length = length ( range) ;
-   KnotSpan = range_length/knots_number;
-end
-
