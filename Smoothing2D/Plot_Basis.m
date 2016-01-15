b324a20811761ad51e78a0a0fb6be73d2bf5c60e
unknown <sduvvuri@imsas95.imsas.uni-bremen.de>
2016-01-15 17:09:43 +0100

    new figures have been added , and codes have been improved

--- Smoothing2D/Plot_Basis.m
+++ Smoothing2D/Plot_Basis.m
@@ -1,9 +1,6 @@
 function [  BS_Val, BS_Hori, BS_Verti] = Plot_Basis( splinesPerAxis,knotsPerAxis,vector,xyMin,xyMax)
-
 vector_length = length(vector);
-
 p=0;
-
 BS_Hori = NaN(vector_length, vector_length);
 BS_Verti = NaN(vector_length, vector_length);
 BS_Val = NaN(vector_length, vector_length);
@@ -13,11 +10,13 @@ for splineNumberHorizontal= 1:splinesPerAxis
         p=p+1;
         q=0;
         for m= 1:vector_length
+            x = vector (m);
+            [horizontal,HorDerv] = calcSpline1D_Single(x, knotsPerAxis, xyMin, xyMax,splineNumberHorizontal);
             for n = 1:vector_length
                 q=q+1;
-                x = vector (m);
+                %x = vector (m);
                 y = vector (n);
-                [horizontal,HorDerv] = calcSpline1D_Single(x, knotsPerAxis, xyMin, xyMax,splineNumberHorizontal);
+                %[horizontal,HorDerv] = calcSpline1D_Single(x, knotsPerAxis, xyMin, xyMax,splineNumberHorizontal);
                 [vertical,VerDerv] = calcSpline1D_Single(y, knotsPerAxis, xyMin, xyMax,splineNumberVertical);
                 BS_Val(p,q) =horizontal*vertical ;
                 BS_Hori(p,q)= vertical*HorDerv;  %%%%%%%%%% Little Change RJ %%%%% [product rule -->(f.g)' = f'.g + g'+f  ]
@@ -27,7 +26,5 @@ for splineNumberHorizontal= 1:splinesPerAxis
         end
     end
 end
-
-
 end
 
--- Smoothing2D/SSOCV_DS1_2D.m
+++ Smoothing2D/SSOCV_DS1_2D.m
@@ -1,12 +1,13 @@
 %Contains code to find optimised smoothing parameter using ordinary cross validation. 
 
+%WITH DATA SET ONE
 
 tic
 clear
 clc
 xyMin = -1;
 xyMax = 1;
-nSensors =300;
+nSensors =70;
 noiseLevel = 0.1;
 lambda_start = [.001,.005,.007];
 lambda_end = .5;
@@ -18,7 +19,7 @@ totalSplines = splinesPerAxis^2;
 knotspan = (xyMax-xyMin)/(knotsPerAxis-1);
 cleanLen=52;
 %splineNumber = 3;
-select =1;
+select_DataSet =2;
 
 xVec = linspace(xyMin,xyMax,cleanLen);
 yVec = linspace(xyMin,xyMax,cleanLen);
@@ -32,9 +33,9 @@ knots = linspace(xyMin,xyMax, knotsPerAxis);
 zzClean = NaN(cleanLen, cleanLen);
 FunctionType =1;
 doEquispaced = 0;
-if select ==1
+if select_DataSet ==1
     [xSensor, ySensor, zClean, zMess, CleanRef] = generateTestData2D(nSensors, noiseLevel, FunctionType, doEquispaced);
-elseif select ==2
+elseif select_DataSet ==2
     [SensorData, CleanData] = loadTestData(nSensors, noiseLevel, 'f', 'r');
     xSensor = SensorData.x;
     ySensor = SensorData.y;
@@ -42,20 +43,30 @@ elseif select ==2
     zClean = SensorData.zClean;
     xVec = CleanData.xVec;
     yVec = CleanData.yVec;
+    zzClean = CleanData.zMatrix;
 end
 
 [xx,yy] = meshgrid(xVec, yVec);
 z=0;
-for i=1:cleanLen
-    for k=1:cleanLen
-        zzClean(i,k)=getHiddenSpatialFunction(xVec(i),yVec(k), FunctionType);
+if select_DataSet ==1
+    for i=1:cleanLen
+        for k=1:cleanLen
+            zzClean(i,k)=getHiddenSpatialFunction(xVec(i),yVec(k), FunctionType);
+        end
     end
-end                                                                                                                                                                               
+end
 figure (1)
-title ( 'Clean data');
+
+surf(xx,yy,zzClean','EdgeColor',[0.7 0.7 0.7],'FaceAlpha',0.5);title ( 'Clean data');
 hold on
-surf(xx,yy,zzClean','EdgeColor',[0.7 0.7 0.7],'FaceAlpha',0.5);
 plot3 ( xSensor , ySensor , zMess ,'r*');
+legend ( 'CleanData', 'Sensors');
+axis([xyMin-0.1 xyMax+0.1 xyMin-0.1 xyMax+0.1 -1.2 1.2])
+text(0.5, 0.7, .75, sprintf('noise = %g',noiseLevel));
+text(0.5, 0.9, .5, sprintf('nSensors %g',nSensors));
+xlabel('x [n]');
+ylabel('y [n]');
+zlabel('z [n]');
 hold off
 %calculating Basis of B-SPlines
 %matrix  BS represents Basis
@@ -74,12 +85,20 @@ end
 %Plotting the regression splines using the calculated weights
 zz=plot_Spline( splinesPerAxis,knotsPerAxis, xVec,yVec,xyMin,xyMax,weights_matrix); % Function to plot the splines
 figure(2)
+
+surf(xx,yy,zz,'EdgeColor',[0.7 0.7 0.7],'FaceAlpha',0.5);title ( 'Clean data');
 title ( 'Regression Spline') ;
+
 hold on
-surf(xx,yy,zz,'EdgeColor',[0.7 0.7 0.7],'FaceAlpha',0.5);
 plot3 ( xSensor , ySensor , zMess ,'r*');
 axis([xyMin-0.1 xyMax+0.1 xyMin-0.1 xyMax+0.1 -1.1 1.1]);
-hold off;
+legend ( 'Predictor', 'Sensors');
+axis([xyMin-0.1 xyMax+0.1 xyMin-0.1 xyMax+0.1 -1.2 1.2])
+text(0.5, 0.7, .75, sprintf('noise = %g',noiseLevel));
+text(0.5, 0.9, .5, sprintf('nSensors %g',nSensors));
+xlabel('x [n]');
+ylabel('y [n]');
+zlabel('z [n]');
 
 
 %--------------------------------------------------
@@ -198,13 +217,26 @@ if lambda_same ==1
     end
     
     zz= plot_Spline( splinesPerAxis,knotsPerAxis, xVec,yVec,xyMin,xyMax,weights_opt_matrix); % function to plot spline
-    figure (5)
+    figure (3)
+    
+    surf (xx,yy,zz,'EdgeColor',[0.7 0.7 0.7],'FaceAlpha',0.5);
+    title ( ' Smoothing Spline parameter choosen manually ' );
     hold on
-    title ('cross Validation implementation');
-    text(0.5, 0.5, 1, sprintf('\\lambda = %g',lambda_new));
-    surf (xx,yy,zz);
+    plot3 ( xSensor , ySensor , zMess ,'r*');
+    legend ( 'Prediction', 'Sensors');
     axis([xyMin-0.1 xyMax+0.1 xyMin-0.1 xyMax+0.1 -1.2 1.2]);
-    hold off
+    %text(0.5, 0.5, 1, sprintf('\\lambda_1= %g \\lambda_2= %g',lambda_start, lambda_end));
+    if lambda_same ==1
+        text(0.5, 0.5, 1, sprintf('\\lambda = %g',lambda_start(1)));
+    else
+        text(0.5, 0.5, 1, sprintf('\\lambda_1 = %g lambda_2 = %g',lambda_start(1), lambda_end));
+    end
+    
+    text(0.5, 0.7, .75, sprintf('noise = %g',noiseLevel));
+    text(0.5, 0.9, .5, sprintf('nSensors %g',nSensors));
+    xlabel('x [n]');
+    ylabel('y [n]');
+    zlabel('z [n]');
 end
 
 toc
